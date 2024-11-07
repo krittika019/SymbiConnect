2ca8214e71c02465283c144ba2f049b7a6db5d62
Shreyansh Saboo <shreyanshsaboo@gmail.com>
2024-11-07 12:08:33 +0530

    change origin in server + setup

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const databaseURL = process.env.DATABASE_URL ;
 
 app.use(
     cors({
-        origin: [process.env.ORIGIN],
+        origin: "http://127.0.0.1:5173",
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
